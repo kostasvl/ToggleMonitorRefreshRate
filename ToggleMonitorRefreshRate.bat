03f97d1114f584dbb6b24ac76f8f2e08b6f019e7
kostasvl <kostasvl@users.noreply.github.com>
2018-11-17 16:34:35 +0000

    More user-friendly messaging from the batch file :)

--- ToggleMonitorRefreshRate.bat
+++ ToggleMonitorRefreshRate.bat
@@ -1,4 +1,21 @@
-ToggleMonitorRefreshRate.exe 100
+@echo off
+
+echo --- Toggle Monitor Refresh Rate ---
+echo.
+
+"%~dp0ToggleMonitorRefreshRate.exe" 100
+
+echo.
+echo Launching app...
+echo.
+
 %1
+
+echo App exited?
 pause
-ToggleMonitorRefreshRate.exe 60
+
+"%~dp0ToggleMonitorRefreshRate.exe" 60
+
+echo.
+echo Done.
+timeout /t 3 > nul
